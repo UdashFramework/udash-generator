6fcb4a435cdd1b393dcc3131c6309c3a95fe52e4
starzu <m.starzec@avsystem.com>
2016-04-22 14:44:20 +0200

    Input validation improvements; closes #8

--- core/src/main/scala/io/udash/generator/configuration/decisions.scala
+++ core/src/main/scala/io/udash/generator/configuration/decisions.scala
@@ -15,9 +15,9 @@ sealed abstract class SelectDecision[ResponseType](default: ResponseType) extend
     if (!options.contains(response.get)) Some(s"Select one of following values: $options") else None
 }
 
-sealed abstract class NoneEmptyStringDecision(errorMsg: String, default: String) extends Decision[String](default) {
+sealed abstract class NonEmptyStringDecision(errorMsg: String, default: String) extends Decision[String](default) {
   override def validator(): Option[String] =
-    if (response.get.isEmpty) Some(errorMsg) else None
+    if (response.get.isEmpty || response.get.trim.isEmpty) Some(errorMsg) else None
 }
 
 /** Project will be generated in this directory. */
@@ -34,15 +34,42 @@ case class RootDirectory(override val response: Option[File] = None) extends Dec
 case class ClearRootDirectory(override val response: Option[Boolean] = None) extends Decision[Boolean](false)
 
 /** Generated project name. */
-case class ProjectName(override val response: Option[String] = None) extends NoneEmptyStringDecision("Project name can not be empty!", "udash-app")
+case class ProjectName(override val response: Option[String] = None) extends NonEmptyStringDecision("Project name cannot be empty!", "udash-app") {
+  override def validator(): Option[String] =
+    super.validator() match {
+      case None =>
+        if ("[^A-Za-z\\d-]".r.findFirstIn(response.get).nonEmpty) Some("Project name can contain only characters, digits and '-' sign.")
+        else None
+      case error => error
+    }
+}
 
 /** Organization name. */
-case class Organization(override val response: Option[String] = None) extends NoneEmptyStringDecision("Organization name can not be empty!", "com.example")
+case class Organization(override val response: Option[String] = None) extends NonEmptyStringDecision("Organization name cannot be empty!", "com.example") {
+  override def validator(): Option[String] =
+    super.validator() match {
+      case None =>
+        if ("^[A-Za-z]+(\\.[A-Za-z][A-Za-z\\d-]*)*$".r.findFirstIn(response.get).isEmpty)
+          Some(
+            """Organization name should be provided in reverse domain notation, eg. 'io.udash'.
+              |The first part can contain only characters, the others can contains characters, digits and '-' sign.
+            """.stripMargin)
+        else None
+      case error => error
+    }
+}
 
 /** Root source code package. */
-case class RootPackage(override val response: Option[Seq[String]] = None) extends Decision[Seq[String]](Seq("com", "example")) {
+case class RootPackage(override val response: Option[Seq[String]] = None) extends Decision[Seq[String]](Seq("com", "example"))  {
   override def validator(): Option[String] =
-    if (response.get.isEmpty) Some("Root package can not be empty!") else None
+    if (response.isEmpty || response.get.isEmpty)
+      Some("Root package can not be empty.")
+    else if (response.get.exists(part => !part.matches("^[A-Za-z][A-Za-z\\d]*$")))
+      Some(
+        """Root package should be provided in reverse domain notation, eg. 'io.udash.app'.
+          |The first part can contain only characters, the others can contains characters and digits.
+        """.stripMargin)
+    else None
 }
 
 /** Frontend-only or standard Udash project configuration. */
@@ -56,8 +83,10 @@ case class StdProjectTypeModulesSelect(override val response: Option[StandardPro
     val project = response.get
     if (project.backend == project.frontend || project.backend == project.shared || project.frontend == project.shared)
       Some("Module names must be unique.")
-    else if (project.backend.isEmpty || project.shared.isEmpty || project.frontend.isEmpty)
+    else if (project.backend.trim.isEmpty || project.shared.trim.isEmpty || project.frontend.trim.isEmpty)
       Some("Module names must not be empty.")
+    else if (Seq(project.backend, project.frontend, project.shared).exists(name => "[^\\w\\d-]".r.findFirstIn(name).nonEmpty))
+      Some("Module name can contain only characters, digits and '-' sign.")
     else None
   }
 }
